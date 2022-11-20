0321d0898cc706c59c934be96abe1b79fe75828a
Wolologger <asierra95@outlook.es>
2022-11-20 18:04:44 +0100

    Añadiendo texto al template
    Incluir @yield('nombre') en la zona
    Y en welcome
    @section('nombre')
    texto a escribir
    @endsection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,6 @@
 @extends('layouts.template')
+
+@section('content')
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, aliquid! Ipsa molestias ipsum optio quasi dignissimos quis rerum nam commodi. Inventore sint, voluptatum quis odit sequi expedita debitis incidunt et.
+
+@endsection
